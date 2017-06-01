8e6dc6fb135413b3364d67af54d30826fbe1ef6f
Gabriele Tomberli <gabriele.tomberli@gmail.com>
2017-06-01 23:28:02 +0200

    Fixed colors in my-console.js

--- src/my-console.js
+++ src/my-console.js
@@ -8,11 +8,12 @@ let console = Console.create( 60 );
 let iff = ( c, t, f ) => c ? t() : f();
 let info = ( m ) => console.print( m, chalk.green );
 let warn = ( m ) => console.print( m, chalk.yellow );
+let error = ( m ) => console.print( m, chalk.red );
 let command = ( m ) => console.print( m, chalk.cyan );
 
-console.info = ( m, c ) => info( m, chalk.green ) & iff( c, () => command( ' ' + c ), _.noop ) & console.line();
-console.warn = ( m, c ) => warn( m, chalk.yellow ) & iff( c, () => command( ' ' + c ), _.noop ) & console.line();
-console.error = ( m, c ) => warn( m, chalk.red ) & iff( c, () => command( ' ' + c ), _.noop ) & console.line();
+console.info = ( m, c ) => info( m ) & iff( c, () => command( ' ' + c ), _.noop ) & console.line();
+console.warn = ( m, c ) => warn( m ) & iff( c, () => command( ' ' + c ), _.noop ) & console.line();
+console.error = ( m, c ) => error( m ) & iff( c, () => command( ' ' + c ), _.noop ) & console.line();
 console.command = ( c ) => command( c ) & console.line();
 console.title = ( m ) => { let hr = _.repeat( '-', m.length ); info( hr + '\n' + m + '\n' + hr + '\n' ); };
 
