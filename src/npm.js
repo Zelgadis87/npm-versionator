a37d169f4e3ff96318e6f73ace5fb4773179ec51
Gabriele Tomberli <gabriele.tomberli@gmail.com>
2017-11-11 16:46:15 +0100

    Removed forced --color option on npm test

--- src/npm.js
+++ src/npm.js
@@ -25,7 +25,7 @@ npm.validate = async function() {
 npm.test = async function( data_out, data_err ) {
 
 	return new Bluebird( ( resolve, reject ) => {
-		let test = child_process.spawn( /^win/.test( process.platform ) ? 'npm.cmd' : 'npm', [ 'test', '--', '--color' ] );
+		let test = child_process.spawn( /^win/.test( process.platform ) ? 'npm.cmd' : 'npm', [ 'test' ] );
 		test.stdout.on( 'data', x => data_out( x.toString() ) );
 		test.stderr.on( 'data', x => data_err( x.toString() ) );
 		test.on( 'close', code => { return code === 0 ? resolve() : reject(); } );
